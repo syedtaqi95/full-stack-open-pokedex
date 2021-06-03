4a0a59099113d40d1ab32f8bf4296baec11d7ebc
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2021-06-03 16:02:58 +0100

    11.17: test commit #skip

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
 const App = () => {
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=784', mapResults)
   if (isLoading) {
-    return <LoadingSpinner />
+    return <LoadingSpinner /> 
   }
   if (error) {
     return <ErrorMessage error={error} />
